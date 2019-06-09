e54b497c175b8b3dd3e964f20721c6333632cb14
baripembo <erikawei@gmail.com>
2019-06-09 10:20:51 -0400

    init

---
+++ app.js
@@ -0,0 +1,4 @@
+window.$ = window.jQuery = require('jquery');
+var slick = require('slick-carousel');
+var scrollstory = require('./node_modules/scrollstory/dist/jquery.scrollstory.js');
+var css = require('./app.css');
